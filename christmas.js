d8a602f26f1a230d91093e2578909e026cf5717e
cam rains <csr66@nau.edu>
2019-05-17 10:15:31 -0700

    newest changes

--- christmas.js
+++ christmas.js
@@ -3,11 +3,11 @@ const christmas = {
     day2: "there",
     day3: "cinderella man",
     day4: "whats good",
-    day5: "hello",
+    day5: "you change something",
     day6: "what dooooo youuuuuu meme",
     day7: "IM ANGRY",
     day8: "ANNNNGRRRYYYYY",
-    day9: "WHAT",
+    day9: "bettter communicate",
     day10: "DO ",
     day11: "YOU",
     day12: "MEME"
