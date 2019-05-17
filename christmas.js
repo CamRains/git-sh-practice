661b3058b446cceb7cd8b0d90e4840b5e7d68410
cam rains <csr66@nau.edu>
2019-05-17 09:47:25 -0700

    boooooooooooo

---
+++ christmas.js
@@ -0,0 +1,14 @@
+const christmas = {
+    day1: "hello",
+    day2: "there",
+    day3: "cinderella man",
+    day4: "",
+    day5: "",
+    day6: "",
+    day7: "",
+    day8: "",
+    day9: "",
+    day10: "",
+    day11: "",
+    day12: ""
+  };
